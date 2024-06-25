d6c77d0a13bac1f7c95845e0da9204809b4d3975
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-24 19:41:14 -0500

    Added app icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+
 import MainLayout from './components/layout/MainLayout';
+
 
 const App: React.FC = () => {
   return (
     <MainLayout>
       <div className="app">
-        <main className="content">
-
-        </main>
+        <main className="content"></main>
       </div>
     </MainLayout>
   );
