dcc5b510e43c3ccf55f76085d54c049c037dc6b4
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-25 19:49:49 -0500

    Fixed types

---
+++ src/types/color.types.ts
@@ -0,0 +1,19 @@
+interface ColorShades {
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+}
+
+interface Colors {
+  grey: ColorShades;
+  primary: ColorShades;
+  greenAccent: ColorShades;
+  redAccent: ColorShades;
+  blueAccent: ColorShades;
+}
