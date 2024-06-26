40875c8aeae8099fd5240e4cbbf98a3ef6785f8d
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-25 19:51:05 -0500

    Added barrel file to manage the mui Icons

---
+++ src/utils/muiIcons.ts
@@ -0,0 +1,23 @@
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+export {
+  DarkModeOutlinedIcon,
+  LightModeOutlinedIcon,
+  NotificationsNoneOutlinedIcon,
+  PersonOutlineOutlinedIcon,
+  SearchOutlinedIcon,
+  SettingsOutlinedIcon,
+  MenuOutlinedIcon,
+  HomeOutlinedIcon,
+  ReceiptIcon,
+  AccountCircleOutlinedIcon,
+}
