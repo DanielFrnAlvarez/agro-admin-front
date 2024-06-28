92f27a989ebf486af6263571fb8fd8fc10cb44d8
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-28 14:43:34 -0500

    Add dotenv to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local', override: true });
+
+const portString = process.env.PORT;
+const port = portString ? parseInt(portString) : 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: port,
+  }
 })
