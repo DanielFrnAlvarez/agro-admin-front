c73de06f5ea685886e45b9ac8520769769f87eec
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-28 14:44:22 -0500

    general fixes

--- src/pages/dashboard/Dashboard.page.tsx
+++ src/pages/dashboard/Dashboard.page.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
   return <Box m="20px">
     <Box display="flex" justifyContent="space-between" alignItems="center">
       <Header title="Inicio" subtitle="Bienvenido" />
